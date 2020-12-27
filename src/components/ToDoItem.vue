9fcc02b060a06161407cbdb157434593885ebbe6
ToLoad <kublaihkan@gmail.com>
2020-12-27 18:07:53 +0900

    Final init

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,21 +1,82 @@
 <template>
-  <div class="custom-checkbox">
-    <input class="checkbox" type="checkbox" :id="id" :checked="isDone" />
-    <label class="checkbox-label" :for="id">{{ label }}</label>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')"
+      />
+      <label :for="id" class="checkbox-label">{{ label }}</label>
+    </div>
+    <div class="btn-group">
+      <button
+        type="button"
+        class="btn"
+        ref="editButton"
+        @click="toggleToItemEditForm"
+      >
+        Edit <span class="visually-hidden">{{ label }}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{ label }}</span>
+      </button>
+    </div>
   </div>
+  <to-do-item-edit-form
+    v-else
+    :id="id"
+    :label="label"
+    @item-edited="itemEdited"
+    @edit-cancelled="editCancelled"
+  ></to-do-item-edit-form>
 </template>
 
 <script>
+import ToDoItemEditForm from "./ToDoItemEditForm";
+
 export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
     id: { required: true, type: String },
   },
+  components: {
+    ToDoItemEditForm,
+  },
   data() {
     return {
-      isDone: this.done,
+      isEditing: false,
     };
+  },
+  computed: {
+    isDone() {
+      return this.done;
+    },
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit("item-edited", newLabel);
+      this.isEditing = false;
+      this.focusOnEditButton();
+    },
+    editCancelled() {
+      this.isEditing = false;
+      this.focusOnEditButton();
+    },
+    focusOnEditButton() { // Edit 버튼을 눌렀을 때 InputBox에 포커스가 가도록함.
+      this.$nextTick(() => {
+        const editButtonRef = this.$refs.editButton;
+        editButtonRef.focus();
+      });
+    },
   },
 };
 </script>
